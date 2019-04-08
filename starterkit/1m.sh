d688c08cec9e2f85d8fad3ecafa66bb9a65d8d27
mech12 <whdnrfo@gmail.com>
2019-04-08 20:37:03 +0900

    start

---
+++ starterkit/1m.sh
@@ -0,0 +1,1 @@
+minerd --user <<rpcuser>> --pass <<rpcpasswd>> --url http://127.0.0.1:9112/ --threads 2 --coinbase-addr 1Bbx9rvGFra8jMLpuHn1Mg2dXAwkQpMG3U --coinbase-sig "test 12ships coin1" -a sha256d -D
